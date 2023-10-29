2c4cfe1e66d3de976964f2f118fc34c2d31d055a
tcbitt <114869382+tcbitt@users.noreply.github.com>
2023-10-29 14:00:20 -0400

    Update Prog4.cpp

--- Prog4.cpp
+++ Prog4.cpp
@@ -7,9 +7,14 @@ void adjustList(int, int[], int, int);
 double calcAverage(int, int[]);
 int* mergeLists(int, int[], int, int[]);
 int* valuesWithinRange(int, int[], int, int);
+int findInList(int, int[], int);
+int findInList(int, int[], int, int);
+int findInList(int, std::string[], std::string);
+int findInList(int, std::string[], std::string, int);
 
-int main()
-{
+int main() {
+    
+    std::string strArr[] = {"0", "7", "7", "4", "12", "10033", "45", "34", "-1", "3"};
     int arr1[10] = {0,7,7,4,12,10033,45,34,-1,3};
     int arr[10] = {0,7,7,4,12,10033,45,34,-1,3};
     std::cout<< findMax(10, arr) << std::endl;
@@ -21,13 +26,24 @@ int main()
     //Test to check merged list
     int* mergePtr = mergeLists(10, arr, 10, arr1);
     for(int i = 0; i < 20; i++)
-        std::cout << *(mregePtr+i) << " ";
+        std::cout << *(mergePtr+i) << " ";
+    
+    std::cout<<std::endl;
     
     //Test to check list range
+    //Having to hard code iteration count to check if the array fills properly
     int* rangePtr = valuesWithinRange(10, arr1, 0, 4);
-    for(int i = 0; i < 10)
-        
-    delete ptr;
+    for(int i = 0; i < 4; i++)
+        std::cout << *(rangePtr+i) << " ";
+    
+    std::cout << std::endl << findInList(10, arr, 7) << std::endl;
+    std::cout << findInList(10, arr1, 3, 4) << std::endl;
+    
+    std::cout<< findInList(10, strArr, "34") << std::endl;
+    std::cout<< findInList(10, strArr, "12", 2);
+    
+    delete mergePtr;
+    delete rangePtr;
     return 0;
 }
 
@@ -81,12 +97,12 @@ int* mergeLists (int size1, int list1[], int size2, int list2[]){
 int* valuesWithinRange(int size, int theList[], int lowLimit, int highLimit){
     int newArrSize = 0, newArrIndex = 0;
     for (int i = 0; i < size; i++)
-        if (theList[i] > lowLimit && theList[i] < highLimit)
+        if (theList[i] >= lowLimit && theList[i] <= highLimit)
             newArrSize++;
     
-    int* rtnArr = new int[count];
+    int* rtnArr = new int[newArrIndex];
     for (int i = 0; i < size; i++)
-        if (theList[i] > lowLimit && theList[i] < highLimit){
+        if (theList[i] >= lowLimit && theList[i] <= highLimit){
             rtnArr[newArrIndex] = theList[i];
             newArrIndex++;
         }
@@ -94,11 +110,34 @@ int* valuesWithinRange(int size, int theList[], int lowLimit, int highLimit){
     return rtnArr;
 }
 
-
 int findInList(int size, int theList[], int valueToFind){
+    for (int i = 0; i < size; i++)
+        if (theList[i] == valueToFind)
+            return i;
     
+    return -1;        
 }
 
 int findInList(int size, int theList[], int valueToFind, int startingIndex){
+    for(int i = startingIndex; i < size; i++)
+        if(theList[i] == valueToFind)
+            return i;
     
+    return -1;
+}
+
+int findInList(int size, std::string theList[], std::string valueToFind){
+    for (int i = 0; i < size; i++)
+        if (theList[i] == valueToFind)
+            return i;
+    
+    return -1;
+}
+
+int findInList(int size, std::string theList[], std::string valueToFind, int startingIndex){
+    for(int i = startingIndex; i < size; i++)
+        if(theList[i] == valueToFind)
+            return i;
+    
+    return -1;
 }
