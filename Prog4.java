71419fabd5c37a5fe416c9b610d894debc603778
tcbitt <114869382+tcbitt@users.noreply.github.com>
2023-10-29 20:00:23 -0400

    Update Prog4.java

--- Prog4.java
+++ Prog4.java
@@ -1,3 +1,9 @@
+/*
+Name: Travis Bittner
+Email: travis.bittner@und.edu
+
+Java array manipulation program
+*/
 public class Prog4
 {
 	public static void main(String[] args) {
@@ -35,7 +41,14 @@ public class Prog4
 		System.out.println(findInList(strArr, "9", 5));
 		System.out.println(findInList(strArr, "10", 5));
     }
-	
+	/*
+	findMax function - iterates through the integer array to find the largest number
+                   and returns it.
+
+	Parameters - an integer array.
+
+	Return value- an integer, the largest number in the array
+	*/  
 	public static int findMax(int[] intList) {
 	    int rtn = 0;
 	    for (int i = 0; i < intList.length; i++) {
@@ -45,7 +58,14 @@ public class Prog4
 	    }
 	    return rtn;
 	}
-	
+	/*
+	findMin function - iterates through the integer array to find the smallest number
+                   and returns it.
+
+	Parameters - an integer array.
+
+	Return value- an integer, the smallest number in the array
+	*/
 	public static int findMin(int[] intList) {
 	    int rtn = 0;
 	    for (int i = 0; i <intList.length; i++) {
@@ -55,7 +75,15 @@ public class Prog4
 	    }
 	    return rtn;
 	}
-	
+	/*
+	adjustList function - Takes two values, an upper and lower limit, and converts
+                      any number that is less than or greater than the limit
+                      to that respective number.
+
+	Parameters - two integers, low and high limits; an integer array
+
+	Return value - void
+*/
 	public static void adjustList(int [] theList, int lowLimit, int highLimit) {
 	    for (int i = 0; i < theList.length; i++) {
 	        if (theList[i] < lowLimit) {
@@ -66,7 +94,14 @@ public class Prog4
 	        }
 	    }
 	}
-	
+	/*
+	calcAverage function - Iterates through the integer array and sums up all the values.
+                       Then it returns the sum/size as a double.
+
+	Parameters - an integer array.
+
+	Return value - a double, the average of the integers.
+	*/
 	public static double calcAverage(int[] intList) {
 	    double rtn = 0;
 	    for(int i: intList) {
@@ -75,7 +110,14 @@ public class Prog4
 	    
 	    return rtn/intList.length;
 	}
+	/*
+	mergeLists function - This creates a new array the size of both input arrays and fills it
+                      with their values consecutively.
+
+	Parameters - two integer arrays.
 	
+	Return value - an integer array, the new merged array.
+	*/
 	public static int[] mergeLists (int[] list1, int[] list2) {
 	    int[] rtnArr = new int[list1.length + list2.length];
 	    
@@ -89,7 +131,15 @@ public class Prog4
 	    
 	    return rtnArr;
 	}
-	
+	/*
+	valuesWithinRange function - Uses two for loops, one to find the size of the value range array;
+                             One to populate the array with the values within the range from
+                             the input array.
+
+	Parameters - two integers, low/high limits; an integer array.
+
+	Return value - an integer array, containing only the values within the range.
+	*/   
 	public static int[] valuesWithinRange (int[] theList, int lowLimit, int highLimit) {
 	    int rtnArrIndex = 0, rtnArrSize = 0;
 	    for(int i: theList){
@@ -109,7 +159,13 @@ public class Prog4
 	    return rtnArr;
 	    
 	}
-	
+	/*
+	findInList function - Iterates through the interger array to find the value requested, return -1 if not found
+
+	Parameters - an integer, the value to find in the array; an integer array.
+
+	Return value - an integer, the index of the value requested.
+	*/  
 	public static int findInList(int[] theList, int valueToFind) {
 	    for(int i = 0; i < theList.length; i++) {
 	        if (theList[i] == valueToFind) {
@@ -119,7 +175,14 @@ public class Prog4
 	    
 	    return -1;
 	}
-	
+	/*
+	findInList function - Iterates through the interger array to find the value requested has the option to use
+ 			      a starting index, return -1 if not found
+
+	Parameters - an integer, the value to find in the array; an integer array; an optional integer, a starting index.
+
+	Return value - an integer, the index of the value requested.
+	*/  
 	public static int findInList(int[] theList, int valueToFind, int startingIndex) {
 	    for(int i = startingIndex; i < theList.length; i++) {
 	        if (theList[i] == valueToFind) {
@@ -129,7 +192,13 @@ public class Prog4
 	    
 	    return -1;
 	}
-	
+	/*
+	findInList function - Iterates through the string array to find the value requested, return -1 if not found.
+
+	Parameters - an integer and a string, the size of the array and the value to find; a string array.
+
+	Return value - an integer, the index of the value requested.
+	*/
 	public static int findInList(String[] theList, String valueToFind) {
 	    for(int i = 0; i < theList.length; i++) {
 	        if (theList[i] == valueToFind) {
@@ -139,7 +208,14 @@ public class Prog4
 	    
 	    return -1;
 	}
-	
+	/*
+	findInList function - Iterates through the string array to find the value requested has the option to use
+  			      a starting index, return -1 if not found.
+
+	Parameters - a string, the value to find; a string array; an optional integer, a starting index.
+
+	Return value - an integer, the index of the value requested.
+	*/
 	public static int findInList(String[] theList, String valueToFind, int startingIndex) {
 	    for(int i = startingIndex; i < theList.length; i++) {
 	        if (theList[i] == valueToFind) {
